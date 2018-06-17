394aff94f6d6704e23e97f236b877ded0f577599
Ignasi Ribó <iribolabastida@gmail.com>
2018-06-17 12:39:09 +0200

    Update setup.py

--- rest_api/setup.py
+++ rest_api/setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'aiodns', 'aiohttp>=2.3.2', 'cchardet', 'protobuf', 'sawtooth-sdk',
-        'pyformance', 'aiohttp_cors'
+        'pyformance', 'aiohttp_cors=>0.5.1'
     ],
     data_files=data_files,
     entry_points={
